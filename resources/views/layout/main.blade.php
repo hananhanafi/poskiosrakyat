5088c1997f645ba74216505ae6a7db440de7f3a2
siti <sitiqotijah01@gmail.com>
2021-10-02 23:53:59 +0700

    ubah title

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -4,7 +4,7 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <link rel="icon" type="image/png" sizes="16x16" href="{{url('img/logokiosrakyat.png')}}">
-  <title>POS | KiosRakyat</title>
+  <title>POS | Kirana</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <!-- Font Awesome -->
